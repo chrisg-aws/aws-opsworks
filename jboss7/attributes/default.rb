817f7439c6e922220ba8f95bf4749ddba8d7df1b
root <root@AmazonLinux.DirBin.com>
2014-04-21 17:45:41 +0000

    jboss7

--- jboss7/attributes/default.rb
+++ jboss7/attributes/default.rb
@@ -1,25 +1,35 @@
 #
-# Cookbook Name:: jboss7
+# Cookbook Name:: jboss
 # Attributes:: default
-# Author:: Sascha Moellering
+# Author:: Bryan W. Berry (<bryan.berry@gmail.com>)
+# Copyright 2011, UN Food and Agriculture Organization
+#
+# license Apache v2.0
+#
 
 
-default['jboss']['home'] = "/srv/jboss"
-default['jboss']['path'] = "jboss-as-7.1.1.Final"
-default['jboss']['version'] = "7.1.1"
-default['jboss']['url'] = "http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip" 
-default['jboss']['tarball'] = "jboss-as-7.1.1.Final.zip"
+default['jboss']['home'] = "/usr/local/jboss"
+default['jboss']['version'] = "7.1.0"
+default['jboss']['url'] = "http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.tar.gz" 
+default['jboss']['checksum'] = "88fd3fdac4f7951cee3396eff3d70e8166c3319de82d77374a24e3b422e0b2ad"
+default['jboss']['version'] = '7.1.1'
 default['jboss']['user'] = "jboss"
 default['jboss']['application'] = 'jboss'
 default['jboss']['config'] = 'standalone'
 default['jboss']['script'] = 'standalone.sh'
 default['jboss']['manage_config_file'] = true
 
-node.override[:java][:jdk_version] = '7'
-node.override[:java][:install_flavor] = "oracle"
-node.override[:java][:oracle][:accept_oracle_download_terms] = true
-
-default['aws']['s3']['access_key'] = ""
-default['aws']['s3']['secret_access_key'] = ""
-default['aws']['s3']['bucket'] = ""
-
+default['jboss']['jdbc']['user'] = 'sa'
+default['jboss']['jdbc']['passwd'] = ''
+default['jboss']['jdbc']['schema'] = ''
+default['jboss']['jdbc']['host'] = 'localhost'
+default['jboss']['jdbc']['transaction_isolation'] = nil
+default['jboss']['jdbc']['driver']['name'] = 'h2'
+default['jboss']['jdbc']['driver']['module'] = 'com.h2database.h2'
+default['jboss']['jdbc']['driver']['version'] = ''
+default['jboss']['jdbc']['driver']['class'] = 'org.h2.jdbcx.JdbcDataSource'
+default['jboss']['datasources'] = []
+default['jboss']['queues'] = []
+default['jboss']['drivers'] = []
+default['jboss']['extra_modules'] = []
+default['jboss']['jms_queues'] = []
--- jboss7/metadata.rb
+++ jboss7/metadata.rb
@@ -1,14 +1,16 @@
-maintainer       "Sascha Moellering"
-maintainer_email "sascha.moellering@gmail.com"
+maintainer       "Bryan Berry"
+maintainer_email "bryan.berry@gmail.com"
 license          "Apache v2.0"
-description      "Installs/Configures JBoss 7.3"
+description      "Installs/Configures jboss"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "1.0.1"
+version          "0.0.13"
+
+%w{ java logrotate }.each do |cb|
+  depends cb
+end
 
 recipe "jboss", "installs jboss from the jboss community site"
 
-%w{ debian ubuntu }.each do |os|
+%w{ debian ubuntu centos redhat fedora }.each do |os|
   supports os
 end
-
-depends "ark"
